f3eaf6783c4e9288fe67c5db07553b5bf40bfea6
Eduardo Estrella <eduardo_1550@hotmail.com>
2018-11-19 12:02:47 -0400

    Registro de Usuario

---
+++ app/js/registrar.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $("#registrarse").click(function(){
+    registrar();
+  })
+})
+
+function registrar() {
+  var username = $("#email").val();
+  var name = $("#nombre").val();
+  var password1 = $("#password1").val();
+  var password2 = $("#password2").val();
+
+  if (password1 != password2) {
+    alert("La contraseña son diferentes")
+  }
+
+  var data = {
+    name: name,
+    email: username, 
+    password: password1
+  }
+  
+  console.log(data);
+  
+  fetch("http://68.183.27.173:8080/register", {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then( res => res.json())
+  .then(response => console.log('Succes:', JSON.stringify(response)))
+  .catch(error => console.error('error', error));
+}
+
