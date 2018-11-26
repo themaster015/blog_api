e9210a17ad7ac579d52926d6f1f363eba92cdea5
Eduardo Estrella <eduardo_1550@hotmail.com>
2018-11-26 00:52:18 -0400

    Arreglos de Link e Icono

--- app/js/post.js
+++ app/js/post.js
@@ -8,52 +8,85 @@ function getPost() {
   var lista = $("#listaPost")
 
   var itemPost = `
-    <div class="card mb-1">
+    <div class="card mb-3">
       <div class="card-header">
-        <h5>{titulo}</h5>
+        <div class="row">
+          <i class="{likePost} id="{likePostId}" fa-star fa-lg mt-1"></i> &nbsp 
+          <h5><a href="#"> {titulo}</a></h5>
+        </div>
       </div>
+
       <div class="card-body">
         
         <div class="text-right">
-          <p>{tags}</p>
-        </div>  
+          {tags}
+        </div>
 
         <p class="card-text">{cuerpo}</p>
-        <h6 class="card-title">By: {user} </h6>
         
-        <div class="text-right">
-          <i class="far fa-star"></i>
-          <p>{likes} stars</p>
+        <div class="row">
+          <div class="col-sm-6">
+            <h6 class="card-title">By: <a href="#">{user}</a> </h6>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="text-right">
+              <i class="far fa-star"></i>
+              <p>{likes} stars</p>
+            </div>
+          </div>
         </div>
-        
+      </div>
+
+      <div class="card-footer text-muted">
+        2 days ago
       </div>
     </div>
   `
   var token = localStorage.getItem("blog_api_user_token");
 
-  fetch("http://68.183.27.173:8080/post", obtenerHeader())
+  fetch(direccionApi + "/post", obtenerHeader())
     .then(res => res.json())
     .then(response => {
       var listaPosts = response;
 
       for (var post of listaPosts) {
+        var tamanoPost = post.body.length;
 
-        var listaTags = post.tags;
-        console.log(listaTags);
-        var tag = ''
+        if (tamanoPost > 150) {
+          var tag = getTags(post.tags);
+          var item = itemPost;
+          item = item.replace('{titulo}', post.title);
+          item = item.replace('{cuerpo}', post.body);
+          item = item.replace('{user}', `${post.userName} (${post.userEmail})`)
+          item = item.replace('{likes}', post.likes);
+          item = item.replace('{tags}', tag);
+          item = item.replace('{likePostId}', `likePost${post.id}`);
 
-        for (const tags of listaTags) {
-          tag += tags + ', '; 
+          if (post.liked) {
+            item = item.replace('{likePost}', 'fa'); 
+          } else {
+            item = item.replace('{likePost}', 'far'); 
+          }
+
+          lista.append(`<li>${item}</li>`);
         }
-        
-        var item = itemPost;
-        item = item.replace("{titulo}", post.title);
-        item = item.replace('{cuerpo}', post.body);
-        item = item.replace('{user}', `${post.userName} (${post.userEmail})`)
-        item = item.replace('{likes}', post.likes);
-        item = item.replace('{tags}', tag);        
-
-        lista.append(`<li>${item}</li>`);    
       }
     }).catch((error) => console.log(error));
+}
+
+function getTags(tags) {
+
+  var htmlTag = `<span class="badge badge-pill badge-primary">{tag}</span> &nbsp`
+  var htmlTag2 = '';
+  var listaTags = tags;
+  var tag = ''
+
+  for (const tags of listaTags) {
+    tag = htmlTag;
+    tag = tag.replace('{tag}', tags)
+    htmlTag2 += tag;
+  }
+
+  return htmlTag2;
 }
